509c1e8ca668e585007b9dfba80a72b8e6e8ab47
fmarin-p <fmarin-p@student.42madrid.com>
2023-10-18 17:55:28 +0200

    tester updated

--- invalid_maps/test.sh
+++ invalid_maps/test.sh
@@ -11,14 +11,14 @@ fi
 { for invalid_map in "$1/"*
 	do
 	echo -n "$invalid_map: "
-	output=$(../cub3D $invalid_map)
-	if [ "$output" = "Error" ]
+	if [ -z $(../cub3D $invalid_map 2>&1 | grep "Error") ]
 	then
-		echo ✓
-	else
 		echo ×
 		echo "$invalid_map:" >> error.txt
-		cat $invalid_map >> error.txt
 		echo >> error.txt
+		cat $invalid_map | grep -m 1 -B 1000 '1111111' >> error.txt
+		echo >> error.txt
+	else
+		echo ✓
 	fi
 done }

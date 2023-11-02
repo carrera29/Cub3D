d3a1551f30b913a8a20b33e85fc80ceb407de34a
fmarin-p <fmarin-p@student.42madrid.com>
2023-11-02 17:59:30 +0100

    checking raycasting

--- src/raycasting.c
+++ src/raycasting.c
@@ -6,7 +6,7 @@
 /*   By: fmarin-p <fmarin-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 17:08:03 by fmarin-p          #+#    #+#             */
-/*   Updated: 2023/11/02 17:10:36 by fmarin-p         ###   ########.fr       */
+/*   Updated: 2023/11/02 17:49:51 by fmarin-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,67 +17,69 @@
 // {
 // }
 
-// int	raycasting(t_cub *cub_data)
-// {
-// 	int i;
-// 	int step[2];
-// 	int map[2];
-// 	double camaraX;
-// 	double raydir[2];
-// 	double sideDist[2];
-// 	double deltaDist[2];
-// 	double plane[2];
+int	raycasting(t_cub *cub_data)
+{
+	int		i;
+	int		step[2];
+	int		map[2];
+	double	cameraX;
+	double	raydir[2];
+	double	side_dist[2];
+	double	delta_dist[2];
+	double	plane[2];
 
-// 	i = -1;
-// 	while (++i < SCREENWIDTH)
-// 	{
-// 		//raycating
-// 		plane[X] = 0, plane[Y] = 0.66;
-// 		camaraX = (i * 2) / (SCREENHEIGHT - 1);
-// 		raydir[X] = cub_data->dir[X] + plane[X] * camaraX;
-// 		raydir[Y] = cub_data->dir[Y] + plane[Y] * camaraX;
+	i = -1;
+	while (++i < SCREENWIDTH)
+	{
+		//raycasting
+		plane[X] = 0;
+		plane[Y] = 0.66;
+		cameraX = (i * 2) / (SCREENWIDTH - 1);
+		raydir[X] = cub_data->dir[X] + plane[X] * cameraX;
+		raydir[Y] = cub_data->dir[Y] + plane[Y] * cameraX;
 
-// 		// calculo deltaDist y sideDist
-// 		if (raydir[X] == 0) 
-// 			deltaDist[X] = fabs(1 / 1e30);
-// 		else
-// 			deltaDist[X] = fabs(1 / raydir[X]);
-// 		if (raydir[X] == 0)
-// 			deltaDist[Y] = fabs(1 / raydir[Y]);
-// 		else
-// 			deltaDist[Y] = fabs(1 / raydir[Y]);
+		// calculo delta_dist y side_dist
+		if (raydir[X] == 0)
+			delta_dist[X] = fabs(1 / 1e30);
+		else
+			delta_dist[X] = fabs(1 / raydir[X]);
+		if (raydir[Y] == 0)
+			delta_dist[Y] = fabs(1 / raydir[Y]);
+		else
+			delta_dist[Y] = fabs(1 / raydir[Y]);
 		
-// 		map[X] = (int)cub_data->pos[X];
-// 		map[Y] = (int)cub_data->pos[Y];
-// 		if (raydir[X] < 0)
-// 			(step[X] = -1, sideDist[X] = (cub_data->pos[X] - map[X]) * deltaDist[X]);
-// 		else
-// 			(step[X] = 1, sideDist[X] = (map[X] + 1 - cub_data->pos[X]) * deltaDist[X]);
-// 		if (raydir[Y] < 0)
-// 			(step[Y] = -1, sideDist[Y] = (cub_data->pos[Y] - map[Y]) * deltaDist[Y]);
-// 		else
-// 			(step[Y] = 1, sideDist[Y] = (map[Y] + 1 - cub_data->pos[Y]) * deltaDist[Y]);
+		map[X] = (int)cub_data->pos[X];
+		map[Y] = (int)cub_data->pos[Y];
+		if (raydir[X] < 0)
+			(step[X] = -1, side_dist[X] = (cub_data->pos[X] - map[X]) * delta_dist[X]);
+		else
+			(step[X] = 1, side_dist[X] = (map[X] + 1 - cub_data->pos[X]) * delta_dist[X]);
+		if (raydir[Y] < 0)
+			(step[Y] = -1, side_dist[Y] = (cub_data->pos[Y] - map[Y]) * delta_dist[Y]);
+		else
+			(step[Y] = 1, side_dist[Y] = (map[Y] + 1 - cub_data->pos[Y]) * delta_dist[Y]);
 
-// 		// step by step
-// 		int hit;
-// 		double perpWallDist;
-// 		hit = 0; 
-// 		while (hit == 0)
-// 		{
-// 			if (sideDist[X] < sideDist[Y])
-// 			{
-// 				sideDist[X] += deltaDist[X];
-// 				map[X] += step[X];
-// 				perpWallDist = (sideDist[X] - deltaDist[X]);
-// 			}
-// 			else
-// 			{
-// 				sideDist[Y] += deltaDist[Y];
-// 				map[Y] += step[Y];
-// 				perpWallDist = (sideDist[Y] - deltaDist[Y]);
-// 			}
-// 			if (cub_data->map_data->map[map[X]][map[Y]] < 0)
-// 				hit = 1;
-// 		}
-// 	}
-// }
+		// step by step
+		int hit;
+		double perpWallDist;
+		hit = 0; 
+		while (hit == 0)
+		{
+			if (side_dist[X] < side_dist[Y])
+			{
+				side_dist[X] += delta_dist[X];
+				map[X] += step[X];
+				perpWallDist = (side_dist[X] - delta_dist[X]);
+			}
+			else
+			{
+				side_dist[Y] += delta_dist[Y];
+				map[Y] += step[Y];
+				perpWallDist = (side_dist[Y] - delta_dist[Y]);
+			}
+			if (cub_data->map_data->map[map[X]][map[Y]] < 0)
+				hit = 1;
+		}
+	}
+	return (EXIT_SUCCESS);
+}

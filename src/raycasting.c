5e38264f8c7733592a0269dd6d5cbf18e4b4c39a
Fran Marín Pérez <franmp97@outlook.com>
2023-11-07 18:47:41 +0100

    step_calculator loop

--- src/raycasting.c
+++ src/raycasting.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   raycasting.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pollo <pollo@student.42.fr>                +#+  +:+       +#+        */
+/*   By: fmarin-p <fmarin-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 17:08:03 by fmarin-p          #+#    #+#             */
-/*   Updated: 2023/11/04 07:33:17 by pollo            ###   ########.fr       */
+/*   Updated: 2023/11/07 18:47:13 by fmarin-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,20 +48,25 @@ int	step_by_step(t_ray *ry, char **map)
 	return (EXIT_SUCCESS);
 }
 
-int	step_calculator(t_ray *ry, char coor, double pos)
+int	step_calculator(t_ray *ry, double *pos)
 {
-	ry->delta_dist[coor] = fabs(1 / ry->raydir[coor]);
-	ry->map[coor] = (int)pos;
+	int	i;
 
-	if (ry->raydir[coor] < 0)
+	i = -1;
+	while (i <= Y)
 	{
-		ry->step[coor] = -1;
-		ry->side_dist[coor] = (pos - ry->map[coor]) * ry->delta_dist[coor];
-	}
-	else
-	{
-		ry->step[coor] = 1;
-		ry->side_dist[coor] = (ry->map[coor] + 1 - pos) * ry->delta_dist[coor];
+		ry->delta_dist[i] = fabs(1 / ry->raydir[i]);
+		ry->map[i] = (int)pos;
+		if (ry->raydir[i] < 0)
+		{
+			ry->step[i] = -1;
+			ry->side_dist[i] = (pos[i] - ry->map[i]) * ry->delta_dist[i];
+		}
+		else
+		{
+			ry->step[i] = 1;
+			ry->side_dist[i] = (ry->map[i] + 1 - pos[i]) * ry->delta_dist[i];
+		}
 	}
 	return (EXIT_SUCCESS);
 }
@@ -81,9 +86,7 @@ int	raycasting(t_cub *cub_data)
 		ry->raydir[Y] = cub_data->dir[Y] + cub_data->plane[Y] * ry->camera;
 		if (ry->raydir[Y] == 0)
 			ry->raydir[Y] = 1e30;
-
 		step_calculator(ry, X, cub_data->pos[X]);
-		step_calculator(ry, Y, cub_data->pos[Y]);
 		
 		step_by_step(ry, cub_data->map_data->map);
 

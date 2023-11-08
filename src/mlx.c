f84bead7583a1254cd270fe739ffcb566d2deb39
Fran Marín Pérez <franmp97@outlook.com>
2023-11-08 10:41:54 +0100

    move working correctly, wall collision pending

--- src/mlx.c
+++ src/mlx.c
@@ -6,27 +6,22 @@
 /*   By: fmarin-p <fmarin-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/31 16:45:34 by fmarin-p          #+#    #+#             */
-/*   Updated: 2023/11/08 02:40:29 by fmarin-p         ###   ########.fr       */
+/*   Updated: 2023/11/08 10:40:27 by fmarin-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	move(t_cub *cub_data, double move_speed, char **map, int dir)
+void	move(t_cub *cub_data, char **map, double dirX, double dirY)
 {
-	const double	table[4][2] = {{0.0, -move_speed}, {0.0, move_speed},
-	{-move_speed, 0.0}, {move_speed, 0.0}};
-	const int		grid_table[4][2] = {{0, -1}, {0, 1}, {-1, 0}, {1, 0}};
-
-	if ((cub_data->pos[X] + table[dir][X] < HITBOX
-		|| cub_data->pos[X] + table[dir][X] > (1 - HITBOX)
-		|| cub_data->pos[Y] + table[dir][Y] < HITBOX
-		|| cub_data->pos[Y] + table[dir][Y] > (1 - HITBOX))
-		&& map[(int)cub_data->pos[Y] + grid_table[dir][Y]]
-		[(int)cub_data->pos[X] + grid_table[dir][X]] == WALL)
-		return ;
-	cub_data->pos[X] += table[dir][X];
-	cub_data->pos[Y] += table[dir][Y];
+	(void) map;
+	// if ((cub_data->pos[X] + cub_data->move_speed < HITBOX
+	// 		|| cub_data->pos[X] + cub_data->move_speed > (1 - HITBOX)
+	// 		|| cub_data->pos[Y] + cub_data->move_speed < HITBOX
+	// 		|| cub_data->pos[Y] + cub_data->move_speed > (1 - HITBOX)))
+	// 	return ;
+	cub_data->pos[X] += dirX * cub_data->move_speed;
+	cub_data->pos[Y] += dirY * cub_data->move_speed;
 }
 
 void	rotate(t_cub *cub_data, double rot_speed)
@@ -46,13 +41,13 @@ void	key_hook(t_cub *cub_data, char **map)
 	if (mlx_is_key_down(cub_data->mlx, MLX_KEY_ESCAPE))
 		mlx_close_window(cub_data->mlx);
 	if (mlx_is_key_down(cub_data->mlx, MLX_KEY_W))
-		move(cub_data, cub_data->move_speed, map, 0);
+		move(cub_data, map, cub_data->dir[X], cub_data->dir[Y]);
 	if (mlx_is_key_down(cub_data->mlx, MLX_KEY_A))
-		move(cub_data, cub_data->move_speed, map, 3);
+		move(cub_data, map, -cub_data->dir[Y], cub_data->dir[X]);
 	if (mlx_is_key_down(cub_data->mlx, MLX_KEY_D))
-		move(cub_data, cub_data->move_speed, map, 2);
+		move(cub_data, map, cub_data->dir[Y], -cub_data->dir[X]);
 	if (mlx_is_key_down(cub_data->mlx, MLX_KEY_S))
-		move(cub_data, cub_data->move_speed, map, 1);
+		move(cub_data, map, -cub_data->dir[X], -cub_data->dir[Y]);
 	if (mlx_is_key_down(cub_data->mlx, MLX_KEY_RIGHT))
 		rotate(cub_data, (-1 * cub_data->rot_speed));
 	if (mlx_is_key_down(cub_data->mlx, MLX_KEY_LEFT))

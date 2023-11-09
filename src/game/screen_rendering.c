0f53b73a06ccfb25672763efd2c67494999f4b5f
Fran Marín Pérez <franmp97@outlook.com>
2023-11-09 15:42:39 +0100

    rendering walls

--- src/game/screen_rendering.c
+++ src/game/screen_rendering.c
@@ -6,11 +6,41 @@
 /*   By: fmarin-p <fmarin-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 12:56:28 by fmarin-p          #+#    #+#             */
-/*   Updated: 2023/11/09 12:56:55 by fmarin-p         ###   ########.fr       */
+/*   Updated: 2023/11/09 15:39:07 by fmarin-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+int	get_pixel(int j, uint8_t *pixels, uint32_t width, int screen_pos)
+{
+	int	pixel;
+
+	pixel = pixels[j * (width * 4) + (screen_pos % (width * 4))] << 24
+		| pixels[j * (width * 4) + (screen_pos % (width * 4)) + 1] << 16
+		| pixels[j * (width * 4) + (screen_pos % (width * 4)) + 2] << 8
+		| pixels[j * (width * 4) + (screen_pos % (width * 4)) + 3];
+	return (pixel);
+}
+
+int	render_wall(t_cub *cub_data, t_ray ry, int screen_pos, int i)
+{
+	mlx_texture_t	texture;
+	double			resize_factor;
+	double			j;
+	int				pixel;
+
+	texture = cub_data->xpm[ry.wall_texture]->texture;
+	resize_factor = 1.0 * texture.height / (ry.end_draw - ry.start_draw);
+	j = 0;
+	while (++i < ry.end_draw)
+	{
+		pixel = get_pixel((int)j, texture.pixels, texture.width, screen_pos);
+		mlx_put_pixel(cub_data->screen, screen_pos, i, pixel);
+		j += resize_factor;
+	}
+	return (i);
+}
 
 int	print_screen(t_cub *cub_data, t_ray ry, int screen_pos)
 {
@@ -26,7 +56,7 @@ int	print_screen(t_cub *cub_data, t_ray ry, int screen_pos)
 			mlx_put_pixel(cub_data->screen, screen_pos, i,
 				cub_data->map_data->floor_color);
 		else
-			mlx_put_pixel(cub_data->screen, screen_pos, i, 0x210d91FF);
+			i = render_wall(cub_data, ry, screen_pos, --i);
 	}
 	return (EXIT_SUCCESS);
 }

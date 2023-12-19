2e6604c8222d3d04931944f915b7a73d3f44bb59
fmarin-p <fmarin-p@student.42madrid.com>
2023-12-19 17:25:35 +0100

    debug() deleted

--- src/main.c
+++ src/main.c
@@ -6,21 +6,16 @@
 /*   By: fmarin-p <fmarin-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/31 10:28:58 by pollo             #+#    #+#             */
-/*   Updated: 2023/11/08 20:57:51 by fmarin-p         ###   ########.fr       */
+/*   Updated: 2023/12/19 17:25:17 by fmarin-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	debug(void)
-{
-}
-
 int	main(int argc, char **argv)
 {
 	t_cub	*cub_data;
 
-	debug();
 	if (argc != 2)
 		error(USAGE_ERROR, false);
 	cub_data = initialization(argv[1]);
